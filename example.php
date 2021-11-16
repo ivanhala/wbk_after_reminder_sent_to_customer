185523b0f639610e63e0bcd53f621f8be1961a72
Ivan <ivan@plugion.org>
2021-11-16 22:35:53 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
 add_action( 'wbk_after_reminder_sent_to_customer', 'my_wbk_after_reminder_sent_to_customer', 10, 1 );
 function my_wbk_after_reminder_sent_to_customer( $data ){
     foreach( $data as $id ) {
-		    $booking = new WBK_Booking( $id );
-		    error_log( $booking->get( 'name' ) );
-		    error_log( $booking->get( 'phone' ) );
-		    error_log( $booking->get( 'email' ) );
+	$booking = new WBK_Booking( $id );
+	error_log( $booking->get( 'name' ) );
+	error_log( $booking->get( 'phone' ) );
+	error_log( $booking->get( 'email' ) );
     }
 }
